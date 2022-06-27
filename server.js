b44d9ff6bda11f86483f9f499806310cf872788f
toremann <50668920+toremann@users.noreply.github.com>
2022-06-27 22:41:20 +0200

    express-graphql test

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const graphql = require('graphql')
+const { GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList } = graphql
+const fakeData = require("./data.json")
+var app = express();
+
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: () => ({
+        id: { type: GraphQLInt },
+        firstName: { type: GraphQLString }
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQuery",
+    fields: {
+        getAllUsers: {
+            type: new GraphQLList(UserType),
+            args: { id: { type: GraphQLInt }},
+            resolve(parent, args) {
+                // DB GET ALL *, FIND etc..
+                return fakeData
+            }
+        }
+    }
+})
+
+const schema = new GraphQLSchema({query: RootQuery})
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
